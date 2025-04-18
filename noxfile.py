4ccd8079d527760082eeb36895e9c5b8953c90b5
Felix Fontein <felix@fontein.de>
2025-04-18 13:16:50 +0200

    Use antsibull-nox.toml instead of defining everying in noxfile. (#93)

--- noxfile.py
+++ noxfile.py
@@ -14,7 +14,6 @@ import nox
 
 try:
     import antsibull_nox
-    import antsibull_nox.sessions
 except ImportError:
     print("You need to install antsibull-nox in the same Python environment as nox.")
     sys.exit(1)
@@ -23,46 +22,7 @@ except ImportError:
 IN_CI = "GITHUB_ACTIONS" in os.environ
 
 
-antsibull_nox.setup(
-    collection_sources={
-        "amazon.aws": "git+https://github.com/ansible-collections/amazon.aws.git,main",
-        "community.aws": "git+https://github.com/ansible-collections/community.aws.git,main",
-        "community.crypto": "git+https://github.com/ansible-collections/community.crypto.git,main",
-        "community.dns": "git+https://github.com/ansible-collections/community.dns.git,main",
-        "community.general": "git+https://github.com/ansible-collections/community.general.git,main",
-        "community.sops": "git+https://github.com/ansible-collections/community.sops.git,main",
-        "community.internal_test_tools": "git+https://github.com/ansible-collections/community.internal_test_tools.git,main",
-        "community.library_inventory_filtering": "git+https://github.com/ansible-collections/community.library_inventory_filtering.git,stable-1",
-    },
-)
-
-antsibull_nox.add_lint_sessions(
-    run_isort=False,
-    run_black=False,
-    run_flake8=False,
-    run_pylint=False,
-    run_yamllint=True,
-    yamllint_config=".yamllint",
-    run_mypy=False,
-)
-
-antsibull_nox.add_docs_check(
-    validate_collection_refs="all",
-)
-
-antsibull_nox.add_license_check()
-
-antsibull_nox.add_extra_checks(
-    run_no_unwanted_files=True,
-    no_unwanted_files_module_extensions=[".py"],
-    no_unwanted_files_yaml_extensions=[".yml"],
-)
-
-antsibull_nox.add_build_import_check(
-    run_galaxy_importer=True,
-)
-
-antsibull_nox.add_ansible_lint()
+antsibull_nox.load_antsibull_nox_toml()
 
 
 # Allow to run the noxfile with `python noxfile.py`, `pipx run noxfile.py`, or similar.
